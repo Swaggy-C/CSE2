e9d249e88a45ba437c595f68c06cd52665f16532
swaggyc <ctp319@lehigh.edu>
2015-08-28 15:39:22 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////
+//Carter Prevost
+//Lab 01
+//Hello World Java Program 
+//
+//
+//
+//
+//Define a class
+public class HelloWorld{
+
+//Add main method
+    public static void main(String[] args)  {
+        
+// Print out statement 
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testtest
+     }
+    
+    
+}
