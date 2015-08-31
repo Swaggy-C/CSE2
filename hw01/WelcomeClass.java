51ab2b822b5a5d91b87bb77cb53c66aa0d98a126
swaggyc <ctp319@lehigh.edu>
2015-08-31 16:27:49 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////////////
+//Carter Prevost
+//CSE2 Section 110
+//August 31, 2015
+//Welcome Class Java Program
+
+
+//Define a class
+public class WelcomeClass{
+    
+//Add main method
+    public static void main(String[] args) {
+        
+//Print the following statement
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-C--T--P--3--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This class is much nicer than my business classes");
+
+    }       //End method and end class 
+}
